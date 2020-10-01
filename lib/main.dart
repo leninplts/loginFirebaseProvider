fc650b45c3632e12f92e8911b6d07af1d97f23d4
Lenin Jose <paltokj@gmail.com>
2020-10-01 17:55:29 -0500

    add bloc delegate

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,72 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:login/login_state.dart';
-import 'package:login/pages/home_page.dart';
-import 'package:login/pages/login_page.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:login/src/bloc/authentication/authentication_bloc.dart';
+import 'package:login/src/repository/user_repository.dart';
 
-void main() async {
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MyApp());
+  final UserRepository userRepository = UserRepository();
+  runApp(
+    BlocProvider(
+      create: (context) =>
+          AuthenticationBloc(userRepository: userRepository)..add(AppStarted()),
+      child: App(userRepository: userRepository),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
+class App extends StatelessWidget {
+  final UserRepository _userRepository;
+
+  App({Key key, @required UserRepository userRepository})
+      : assert(userRepository != null),
+        _userRepository = userRepository,
+        super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<LoginState>(
-      create: (BuildContext context) => LoginState(),
-      child: MaterialApp(
-        title: 'Material App',
-        debugShowCheckedModeBanner: false,
-        routes: {
-          '/': (BuildContext context) {
-            var state = Provider.of<LoginState>(context);
-            if (state.isLoggedIn()) {
-              return HomePage();
-            } else {
-              return LoginPage();
-            }
-          },
-          '/HomePage': (context) => HomePage(),
-        },
-      ),
+    return Container(
+      child: child,
     );
   }
 }
+
+/*
+  Login With providers
+*/
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:flutter/material.dart';
+// import 'package:login/login_state.dart';
+// import 'package:login/pages/home_page.dart';
+// import 'package:login/pages/login_page.dart';
+// import 'package:provider/provider.dart';
+
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp();
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return ChangeNotifierProvider<LoginState>(
+//       create: (BuildContext context) => LoginState(),
+//       child: MaterialApp(
+//         title: 'Material App',
+//         debugShowCheckedModeBanner: false,
+//         routes: {
+//           '/': (BuildContext context) {
+//             var state = Provider.of<LoginState>(context);
+//             if (state.isLoggedIn()) {
+//               return HomePage();
+//             } else {
+//               return LoginPage();
+//             }
+//           },
+//           '/HomePage': (context) => HomePage(),
+//         },
+//       ),
+//     );
+//   }
+// }
